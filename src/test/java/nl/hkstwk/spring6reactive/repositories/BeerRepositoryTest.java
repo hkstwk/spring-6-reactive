594e528bf69242a03a90625145def6d4578c0480
hkstwk <hkolvoort@gmail.com>
2024-07-12 20:12:19 +0200

    266/267 - Test Save New Entity/Add Create and Update Date with Auditing

---
+++ src/test/java/nl/hkstwk/spring6reactive/repositories/BeerRepositoryTest.java
@@ -0,0 +1,33 @@
+package nl.hkstwk.spring6reactive.repositories;
+
+import nl.hkstwk.spring6reactive.config.DatabaseConfig;
+import nl.hkstwk.spring6reactive.domain.Beer;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.context.annotation.Import;
+
+import java.math.BigDecimal;
+
+@DataR2dbcTest
+@Import(DatabaseConfig.class)
+class BeerRepositoryTest {
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testSaveNewBeer() {
+        beerRepository.save(getTestBeer()).subscribe(System.out::println);
+    }
+
+    private Beer getTestBeer(){
+        return Beer.builder()
+                .beerName("Karmeliet")
+                .beerStyle("TRIPLE")
+                .upc("abcde")
+                .price(BigDecimal.valueOf(13.75))
+                .quantityOnHand(10)
+                .build();
+    }
+}
