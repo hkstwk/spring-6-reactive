6dd41c8da2cb540cdff3a0b007286556ac5f5289
hkstwk <hkolvoort@gmail.com>
2024-07-12 17:57:50 +0200

    263 - Initializing Database

---
+++ src/main/resources/db/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS beer (
+    id                 integer not null primary key auto_increment,
+    beer_name          varchar(255),
+    beer_style         varchar(255),
+    upc                varchar(25),
+    quantity_on_hand   integer,
+    price              decimal,
+    created_date       timestamp,
+    last_modified_date timestamp
+);
