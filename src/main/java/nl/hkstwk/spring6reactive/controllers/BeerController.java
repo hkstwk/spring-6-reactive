1364cc52389be9aacec44eab38d809e440bab0e1
hkstwk <hkolvoort@gmail.com>
2024-07-14 18:40:32 +0200

    273 - Create New Beer Entity

--- src/main/java/nl/hkstwk/spring6reactive/controllers/BeerController.java
+++ src/main/java/nl/hkstwk/spring6reactive/controllers/BeerController.java
@@ -3,9 +3,12 @@ package nl.hkstwk.spring6reactive.controllers;
 import lombok.RequiredArgsConstructor;
 import nl.hkstwk.spring6reactive.model.BeerDTO;
 import nl.hkstwk.spring6reactive.services.BeerService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -28,5 +31,13 @@ public class BeerController {
         return beerService.getBeerById(beerId);
     }
 
-
+    @PostMapping(BEER_PATH)
+    Mono<ResponseEntity<Void>> newBeer(BeerDTO beerDTO){
+        return beerService.saveNewBeer(beerDTO)
+                .map(savedDto -> ResponseEntity.created(UriComponentsBuilder
+                        .fromHttpUrl("http://localhost:8080/" + BEER_PATH
+                                + "/" + savedDto.getId())
+                        .build().toUri())
+                        .build());
+    }
 }
