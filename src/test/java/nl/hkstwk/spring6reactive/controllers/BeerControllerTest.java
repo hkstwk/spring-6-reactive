3a56a5c3b83f8f24d11228944ca8a2191e7128ee
hkstwk <hkolvoort@gmail.com>
2024-07-18 11:04:52 +0200

    282 - Test Create Beer Entity

--- src/test/java/nl/hkstwk/spring6reactive/controllers/BeerControllerTest.java
+++ src/test/java/nl/hkstwk/spring6reactive/controllers/BeerControllerTest.java
@@ -2,11 +2,13 @@ package nl.hkstwk.spring6reactive.controllers;
 
 import nl.hkstwk.spring6reactive.domain.Beer;
 import nl.hkstwk.spring6reactive.model.BeerDTO;
+import nl.hkstwk.spring6reactive.repositories.BeerRepositoryTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -35,5 +37,15 @@ class BeerControllerTest {
                 .expectStatus().isOk()
                 .expectHeader().valueEquals("Content-Type", "application/json")
                 .expectBody(BeerDTO.class);
+    }
+
+    @Test
+    void testCreateBeer() {
+        webTestClient.post().uri(BeerController.BEER_PATH)
+                .body(Mono.just(BeerRepositoryTest.getTestBeer()), BeerDTO.class)
+                .header("Content-Type", "application/json")
+                .exchange()
+                .expectStatus().isCreated()
+                .expectHeader().location("http://localhost:8080/api/v2/beer/4");
     }
 }
