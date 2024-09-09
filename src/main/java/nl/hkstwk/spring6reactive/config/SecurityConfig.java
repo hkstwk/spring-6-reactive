e355f130b5ec275f50ddeb674c46fa6f20c5c0a6
Harm Kolvoort <hkolvoort+github@gmail.com>
2024-09-09 16:35:15 +0200

    395 - Add Actuator to WebFlux

--- src/main/java/nl/hkstwk/spring6reactive/config/SecurityConfig.java
+++ src/main/java/nl/hkstwk/spring6reactive/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package nl.hkstwk.spring6reactive.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -12,6 +13,15 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 public class SecurityConfig {
 
     @Bean
+    @Order(1)
+    public SecurityWebFilterChain actuatorSecurityFilterChain(ServerHttpSecurity http) throws Exception{
+        http.securityMatcher(org.springframework.boot.actuate.autoconfigure.security.reactive.EndpointRequest.toAnyEndpoint())
+                .authorizeExchange(authorize -> authorize.anyExchange().permitAll());
+        return http.build();
+    }
+
+    @Bean
+    @Order(2)
     SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         http.csrf(ServerHttpSecurity.CsrfSpec::disable).authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec
                         .anyExchange()
